c1b62fffa659d1ca1f01db315a66b315ae054c50
MTue2023 <{ID}+{username}@users.noreply.github.com>
2024-01-21 13:02:29 +0700

    Merge remote-tracking branch 'origin/master'

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/mongkolchaithongchai/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mongkolchaithongchai/my-project/flutter-src/dicee-flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mongkolchaithongchai/my-project/flutter_src/dicee_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
